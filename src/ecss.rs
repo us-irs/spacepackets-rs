b93950bb2e3f740f30fa88a11867249ec0e1a848
Robin Mueller <robin.mueller.m@gmail.com>
2022-07-31 13:32:11 +0200

    added some full stops

--- src/ecss.rs
+++ src/ecss.rs
@@ -7,11 +7,11 @@ use serde::{Deserialize, Serialize};
 
 pub type CrcType = u16;
 
-/// CRC algorithm used by the PUS standard
+/// CRC algorithm used by the PUS standard.
 pub const CRC_CCITT_FALSE: Crc<u16> = Crc::<u16>::new(&CRC_16_IBM_3740);
 pub const CCSDS_HEADER_LEN: usize = size_of::<crate::zc::SpHeader>();
 
-/// All PUS versions. Only PUS C is supported by this library
+/// All PUS versions. Only PUS C is supported by this library.
 #[derive(PartialEq, Copy, Clone, Serialize, Deserialize, Debug)]
 pub enum PusVersion {
     EsaPus = 0,
